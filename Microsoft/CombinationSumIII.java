6fceb6dcaf376097a6c8516e0137aa6b14c83add
Abhiraj <abhirajsinha25@gmail.com>
2023-01-03 00:24:29 +0530

    Day2

--- Microsoft/CombinationSumIII.java
+++ Microsoft/CombinationSumIII.java
@@ -1,26 +1,21 @@
 public class CombinationSumIII{
     List<List<Integer>> ans;
-    public void combinationSum3(int k, int num, int n, int number, ArrayList<Integer> seq) {
-        if (num > k || number > n) {
-            return;
-        }
-
-        if (n == 0) {
-            ans.add(seq);
-            return;
-        }
-
-        int v = n - number;
-        System.out.print(v + " ");
-        seq.add(v);
-        combinationSum3(k, num + 1, n - number, number + 1, seq);
-        seq.remove(seq.size() - 1);
-    }
-
     public List<List<Integer>> combinationSum3(int k, int n) {
         ans = new ArrayList<>();
-        ArrayList<Integer> seq = new ArrayList<>();
-        combinationSum3(k, 1, n, 1, seq);
+        findCombinations(1, k, n, new ArrayList<Integer>());
         return ans;
+    }
+
+    public void findCombinations(int start, int k, int n, ArrayList<Integer> res) {
+        if (n == 0 && k == 0) {
+            ans.add(new ArrayList<>(res));
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            res.add(i);
+            findCombinations(i + 1, k - 1, n - i, res);
+            res.remove(res.size() - 1);
+        }
     }
 }
